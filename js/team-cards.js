2db9e9c05f48a3fb7e568001c8743f281359ebc0
kjin <kjin@mit.edu>
2021-03-11 21:33:19 -0500

    sp21 update

--- js/team-cards.js
+++ js/team-cards.js
@@ -2,19 +2,19 @@ const teamCards = document.querySelectorAll('.team');
 let backgroundActive = [false, false, false, false];
 const teamDescriptions = [
     "Vision Team is responsible for discerning big-picture vision for the fellowship and overseeing the implementation of vision through the various ministries and spaces of ACF. Vision members are the eyes of the chapter. By what we see, we direct other parts of the ministry to serve and lead.",
-    "The primary vision for the team is to glorify and magnify God through scripture-based music, and by the power of the Holy Spirit encourage growth in the spiritual walk of our community.  In worship, we lead the rest of the fellowship in the celebration, of His hope, joy, and peace.",
+    "The purpose of CG Team is to shepherd each CG in becoming the kind of community that God has called us to be, growing in making disciples of one another, walking alongside each other, and building each other up in encouragement, prayer, and love.",
     "Large group team is responsible for casting vision for, planning, and leading large groups, the regular space in ACF in which the entire fellowship meets together to grow in and live out our relationship with God and community.",
-    "The purpose of outreach team is to share God’s relentless love in our lives with students on campus, personally invite them to our community, guide seekers and new Christians in their exploration of faith, and cultivate a passion in and equip ACF to reach out to and share the gospel with the MIT and Boston community.",
-    "Discipleship, After Large Group Activities, Care packages, you name it, we got it! As members of the body, Christ calls us to embrace others who were once strangers as our own. We’re a group of people excited to see God work through relationships within the community and our goal is simple: we want to help you experience the joy that comes from growing with your brothers and sisters.",
-    "We’re all called to worship as believers. While we all express worship in different ways and there may be no one “right way” of worship, one thing is clear: the Word consistently points toward music as an outlet for our love and joy for God. As a team, we use our different voices and instruments as tools to bring our brothers and sisters together to celebrate His goodness as one.",
+    "The purpose of outreach team is to share God's relentless love in our lives with students on campus, personally invite them to our community, guide seekers and new Christians in their exploration of faith, and cultivate a passion in and equip ACF to reach out to and share the gospel with the MIT and Boston community.",
+    "Discipleship, After Large Group Activities, Care packages, you name it, we got it! We're a group of people excited to see God work through relationships within the community and our goal is simple: we want to help you experience the joy that comes from growing with your brothers and sisters.",
+    "While we all express worship in different ways and there may be no one 'right way' of worship, one thing is clear: the Word consistently points toward music as an outlet for our love and joy for God. As a team, we use our different voices and instruments as tools to bring our brothers and sisters together to celebrate His goodness as one.",
 ]
 const teamMemberLists = [
-    "Greg Hsu, Part-Time Staff <br> Austin Wang, President <br> Brian Park, Treasurer <br> Grace Lam, UCO Representative",
-    "Luke Shimanuki (lead) <br> Chris Wang <br> Daniel Stein <br> Erica Weng <br> Shirley Cao <br> William Luo",
-    "Austin Wang (lead) <br> Annie Zhang (assistant lead) <br> Lena Zhu <br> Madi Wong",
-    "Grace Lam (lead) <br> Abraham Skandera (assistant lead) <br> Cindy Wang <br> Doreen Chin",
-    "Brian Park (lead) <br> Kathryn Jin (assistant lead) <br> Chris Cheung <br> Jacqueline Liao <br> Sally Liu <br> Soomin Chun",
-    "Brian Park (co-lead) <br> Wendy Bae (co-lead) <br> Austin Wang (co-lead) <br> Chris Cheung <br> Cindy Wang <br> Doreen Chin <br> Madi Wong",
+    "Brian Park, President <br> Abraham Skandera, UCO Rep <br> Annie Zhang, Treasurer <br> Kathryn Jin, Secretary <br> Ruth Wong, Volunteer Staff <br> Regan Wong, Volunteer Staff",
+    "Ruth Wong (lead) <br> Chelsea Watanabe <br> Daniel Stein <br> Diana Voronin <br> Lucy Zhao <br> Shirley Cao <br> William Luo",
+    "Annie Zhang (lead) <br> Cindy Wang <br> Hana Ro <br> Jacky Luong <br> Regan Wong <br> Ryan Kim",
+    "Abraham Skandera (lead) <br> Angel Yang <br> Doreen Chin <br> Jan Park <br> Melody Guo <br> Nathan Kim",
+    "Kathryn Jin (lead) <br> Aslan Cook <br> Christopher Cheung <br> Erica Liu <br> Xinyu Lin",
+    "Brian Park (lead) <br> Erica Liu <br> Doreen Chin <br> Irene Huang <br> Madi Wong <br> Melody Guo <br> Ryan Kim <br> William Luo",
 ]
 
 teamCards.forEach(switchBackground);
